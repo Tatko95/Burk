9939d22b68aa8e6de728a205dd26e5be50560ad4
Palamarchuk <studypalamarchuk@ukr.net>
2016-10-07 11:17:29 +0300

    Base Service

---
+++ Burk.Core/Service/BaseService.cs
@@ -0,0 +1,51 @@
+﻿using Burk.Core.Repository;
+using System;
+using System.Linq;
+using System.Linq.Dynamic;
+
+namespace Burk.Core.Service
+{
+    public abstract class BaseService<T> : IBaseService<T> where T : class
+    {
+        #region Fields
+        protected IBaseRepository repository;
+        #endregion
+
+        #region CTOR
+        public BaseService(IBaseRepository repo)
+        {
+            repository = repo;
+        }
+        #endregion
+
+        #region Methods
+        public T GetById(string idName, string idValue)
+        {
+            int idValueInt;
+            object idValueObj;
+            if (int.TryParse(idValue, out idValueInt))
+                idValueObj = idValueInt;
+            else
+                idValueObj = idValue;
+
+            T result = repository.Table<T>().Where(idName + " == @0", idValueObj).FirstOrDefault();
+            return result;
+        }
+
+        public void Delete(T obj)
+        {
+            repository.Delete(obj);
+        }
+
+        public T Insert(T obj)
+        {
+            return repository.Insert(obj);
+        }
+
+        public void Update(T obj)
+        {
+            repository.Update(obj);
+        }
+        #endregion
+    }
+}
---
+++ Burk.Core/Service/IBaseService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Burk.Core.Service
+{
+    public interface IBaseService<T> where T : class
+    {
+        T GetById(string idName, string idValue);
+        T Insert(T obj);
+        void Update(T obj);
+        void Delete(T obj);
+    }
+
+    public interface IBaseService
+    {
+        T GetById<T>(string idName, string idValue) where T : class;
+        T Insert<T>(T obj) where T : class;
+        void Update<T>(T obj) where T : class;
+        void Delete<T>(T obj) where T : class;
+    }
+}
