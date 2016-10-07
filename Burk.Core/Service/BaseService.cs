f06fa2e19a897a5da75492216539abdc21bb54cb
Palamarchuk <studypalamarchuk@ukr.net>
2016-10-07 11:25:42 +0300

    До попереднього

--- Burk.Core/Service/BaseService.cs
+++ Burk.Core/Service/BaseService.cs
@@ -48,4 +48,48 @@ namespace Burk.Core.Service
         }
         #endregion
     }
+
+    public abstract class BaseService : IBaseService
+    {
+        #region Fields
+        protected IBaseRepository repository;
+        #endregion
+
+        #region CTOR
+        public BaseService(IBaseRepository repo)
+        {
+            repository = repo;
+        }
+        #endregion
+
+        #region Methods
+        public T GetById<T>(string idName, string idValue) where T : class
+        {
+            int idValueInt;
+            object idValueObj;
+            if (int.TryParse(idValue, out idValueInt))
+                idValueObj = idValueInt;
+            else
+                idValueObj = idValue;
+
+            T result = repository.Table<T>().Where(idName + " == @0", idValueObj).FirstOrDefault();
+            return result;
+        }
+
+        public void Delete<T>(T obj) where T : class
+        {
+            repository.Delete<T>(obj);
+        }
+
+        public T Insert<T>(T obj) where T : class
+        {
+            return repository.Insert<T>(obj);
+        }
+
+        public void Update<T>(T obj) where T : class
+        {
+            repository.Update<T>(obj);
+        }
+        #endregion
+    }
 }
