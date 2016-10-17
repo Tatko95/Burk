33fdc6fc41e7cf2825d8e58dac7698c9a81b4387
Palamarchuk <studypalamarchuk@ukr.net>
2016-10-17 20:32:55 +0300

    Файли локалізації + базовий контролер з реалізацією локалізації + видалення лишнього з апп конфігів

---
+++ Burk.Model/Misc/Enums.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Burk.Model.Misc
+{
+    public enum Language
+    {
+        ua = 1,
+        ru = 2,
+        en = 3
+    }
+}
---
+++ Burk.WebUI/Helpers/CultureHelper.cs
@@ -0,0 +1,51 @@
+﻿using Burk.Model.Misc;
+using System;
+using System.Globalization;
+using System.Threading;
+using System.Web.SessionState;
+
+namespace Burk.WebUI.Helpers
+{
+    public class CultureHelper
+    {
+        protected HttpSessionState session;
+
+        //constructor   
+        public CultureHelper(HttpSessionState httpSessionState)
+        {
+            session = httpSessionState;
+        }
+        // Properties  
+        public static int CurrentCulture
+        {
+            get
+            {
+                if (Thread.CurrentThread.CurrentUICulture.Name == "en-GB")
+                    return (int)Language.en;
+                else if (Thread.CurrentThread.CurrentUICulture.Name == "uk-UA")
+                    return (int)Language.ua;
+                else if (Thread.CurrentThread.CurrentUICulture.Name == "ru-RU")
+                    return (int)Language.ru;
+                return 0;
+            }
+            set
+            {
+                switch (value)
+                {
+                    case ((int)Language.en):
+                        Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-GB");
+                        break;
+                    case ((int)Language.ru):
+                        Thread.CurrentThread.CurrentUICulture = new CultureInfo("ru-RU");
+                        break;
+                    case ((int)Language.ua):
+                        Thread.CurrentThread.CurrentUICulture = new CultureInfo("uk-UA");
+                        break;
+                    default:
+                        break;
+                }
+                Thread.CurrentThread.CurrentCulture = Thread.CurrentThread.CurrentUICulture;
+            }
+        }
+    }
+}
