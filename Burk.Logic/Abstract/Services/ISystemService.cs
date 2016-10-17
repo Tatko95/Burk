50f16ca5ec566ff96bdc5abbdb02ae34cc87354a
Palamarchuk <studypalamarchuk@ukr.net>
2016-10-17 13:57:40 +0300

    Початок наробки сервісу систем.

---
+++ Burk.Logic/Abstract/Services/ISystemService.cs
@@ -0,0 +1,17 @@
+﻿using Burk.Core.Service;
+using Burk.Model.Users;
+using System;
+
+namespace Burk.Logic.Abstract.Services
+{
+    public interface ISystemService : IBaseService<Model.UDB.System>
+    {
+        /// <summary>
+        /// Создание новой системы
+        /// </summary>
+        /// <param name="obj">система</param>
+        /// <param name="user">пользователь, который создает систему</param>
+        /// <returns>созданая система</returns>
+        Model.UDB.System Insert(Model.UDB.System obj, User user);
+    }
+}
---
+++ Burk.Logic/Concrete/Services/SystemService.cs
@@ -0,0 +1,61 @@
+﻿using Burk.Core.Service;
+using Burk.Logic.Abstract.Repositories;
+using Burk.Logic.Abstract.Services;
+using Burk.Model.Users;
+using System;
+using System.Linq;
+using Burk.Model.UDB;
+
+namespace Burk.Logic.Concrete.Services
+{
+    public class SystemService : BaseService<Model.UDB.System>, ISystemService, IBaseService<Model.UDB.System>
+    {
+        #region CTOR
+        public SystemService(IBurkModelRepository repo) : base(repo) { }
+        #endregion
+
+        #region Methods
+        public Model.UDB.System Insert(Model.UDB.System obj, User user)
+        {
+            try
+            {
+                repository.BeginTransaction();
+                var system = base.Insert(obj);
+                UserInSystem userInSystem = new UserInSystem() { System = system, User = user };
+                repository.Insert(userInSystem);
+                
+                //пользователю добавить роль "Создатель системы"
+                repository.Commit();
+                return system;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error in Insert System! See innerException", ex);
+            }
+        }
+
+        public override void Delete(Model.UDB.System obj)
+        {
+            try
+            {
+                repository.BeginTransaction();
+                IQueryable<UserInSystem> userInSystem = repository.Table<UserInSystem>();
+                foreach (var user in userInSystem)
+                    repository.Delete(user);
+                IQueryable<Role> roleInSystem = from rInS in repository.Table<RoleInSystem>().Where(x => x.SystemId == obj.SystemId)
+                                   from r in repository.Table<Role>().Where(x => x.Id == rInS.RoleId)
+                                   select r;
+                foreach (var role in roleInSystem)
+                    repository.Delete(role);
+                base.Delete(obj);
+                repository.Commit();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        #endregion
+    }
+}
