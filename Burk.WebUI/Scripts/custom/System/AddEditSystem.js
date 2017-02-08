bd479f68090c562f80473382a1fd901277ddd29a
Андрей <@PALAMAR-LAPTOP>
2017-02-08 13:08:37 +0200

    Виправлений баг з валідацією + добавлені іконки в грід

--- Burk.WebUI/Scripts/custom/System/AddEditSystem.js
+++ Burk.WebUI/Scripts/custom/System/AddEditSystem.js
@@ -2,9 +2,9 @@
 });
 
 function Save() {
-    //var data = JSON.stringify({ model: GetFormObject('addEditSystemDialog') });
+    var data = JSON.stringify({ model: GetFormObject('addEditSystemDialog') });
     if ($('#addEditSystemDialog').valid()) {
-        //ShowBlockUI();
+        ShowBlockUI();
         $.ajax({
             type: "POST",
             url: '/System/AddEdit/',
@@ -13,24 +13,23 @@ function Save() {
             success: function (text) {
                 var result = text.split(' ')[0];
                 if (result == "SuccessCreate") {
-                    //UnblockUI();
+                    UnblockUI();
                     ShowMessageBox(1, "SuccessfulCreateDiv", localization.SucCreateSystem, function () { window.location.href = "/SettingSystem/Index?systemId=" + text.split(' ')[1] });
                     $('#addEditDialog').dialog('destroy').html("");
                 }
                 else if (result == "SuccessEdit") {
                     ShowMessageBox(1, "SuccessfulCreateDiv", localization.Saved, function () { $("#jqxgrid").jqxGrid('updatebounddata'); });
-                    //UnblockUI();
+                    UnblockUI();
                     $('#addEditDialog').dialog('destroy').html("");
                 }
                 else if (result == "Error") {
-                    //UnblockUI();
+                    UnblockUI();
                     ShowMessageBox(1, "ErrorDiv", localization.ErrorDeveloper);
                 }
             }
         });
     }
     else {
-        alert("WORK");
         ShowMessageBox(1, "ErrorInputDiv", localization.ReqError);
     }
 }
--- Burk.WebUI/Scripts/custom/System/UserSystems.js
+++ Burk.WebUI/Scripts/custom/System/UserSystems.js
@@ -28,26 +28,49 @@ function InitGrid() {
             columns: [
                 { datafield: 'SystemId', hidden: true },
                 {
-                    text: '', datafield: 'Edit', columntype: 'button', width: 110, cellsrenderer: function () {
-                        return localization.Edit;
-                    }, buttonclick: function (row) {
-                        var systemId = $("#jqxgrid").jqxGrid('getrowid', row);
-                        AddEdit(systemId);
+                    text: '', datafield: 'Work', columntype: 'button', width: 30, cellsrenderer: function () {
+                        return '<div style="width: 100%; height: 100%"><img src="/../Content/JQWidgets/images/search.png" style="margin-left: -7px; margin-top: -18px"/></div>';
+                    }
+                },
+                {
+                    text: '', datafield: 'Edit', columntype: 'button', width: 30, cellsrenderer: function () {
+                        return '<div style="width: 100%; height: 100%"><img src="/../Content/JQWidgets/images/icon-edit.png" style="margin-left: -12px; margin-top: -18px"/></div>';
+                    }
+                },
+                {
+                    text: '', datafield: 'Settings', columntype: 'button', width: 30, cellsrenderer: function () {
+                        return '<div style="width: 100%; height: 100%"><img src="/../Content/JQWidgets/images/icon-settings.png" style="margin-left: -8px; margin-top: -18px"/></div>';
                     }
                 },
                 { text: localization.FullName, datafield: 'FullName', cellsrenderer: cellsrenderer, align: 'center', width: 350 },
                 { text: localization.ShortName, datafield: 'ShortName', cellsrenderer: cellsrenderer, align: 'center', width: 350 },
                 {
-                    text: '', datafield: 'Delete', columntype: 'button', width: 19, cellsrenderer: function () {
-                        return "x";
-                    }, buttonclick: function (row) {
-                        var systemId = $("#jqxgrid").jqxGrid('getrowid', row);
-                        Delete(systemId);
+                    text: '', datafield: 'Delete', columntype: 'button', width: 30, cellsrenderer: function () {
+                        return '<div style="width: 100%; height: 100%"><img src="/../Content/JQWidgets/images/icon-delete.png" style="margin-left: -9px; margin-top: -18px"/></div>';
                     }
                 },
             ],
         });
+
+    $("#jqxgrid").on("cellclick", function (event) {
+        var rowindex = event.args.rowindex;
+        var columnindex = event.args.columnindex;
+        var systemId = $("#jqxgrid").jqxGrid('getrowid', rowindex);
+        if (columnindex == 1) {
+            alert("Work");
+        }
+        if (columnindex == 2) {
+            AddEdit(systemId);
+        }
+        if (columnindex == 3) {
+            window.location.href = "/SettingSystem/Index?systemId=" + systemId;
+        }
+        if (columnindex == 6) {
+            Delete(systemId);
+        }
+    });
 }
+
 
 function AddEdit(objKey) {
     $.ajax({
@@ -57,7 +80,7 @@ function AddEdit(objKey) {
         success: function (result) {
             $("#addEditDialog").html(result).dialog({
                 width: 220,
-                height: 220,
+                height: 'auto',
                 resizable: false,
                 modal: true,
                 show: { effect: "drop", direction: "left" },
